cf371f1e0ffc6fd6a70c12c8c8aa89ef8ed908e8
Abubakar Javed <abubakarjaved03145005454@gmail.com>
2024-07-27 21:29:38 +0500

    1. Added Basic Table Component Structure 2. Added declaration file for ts to detect css modules files as well.

---
+++ RMS/src/Components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Table.module.css"
+function Table(){
+
+    return(
+        <main className={styles.main_body}>
+
+        <span>This is the test table</span>
+        </main>
+    )
+}
+
+export default Table;
--- RMS/src/Pages/Dasboard.jsx
+++ RMS/src/Pages/Dasboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Table from '../Components/Table/Table'
 
 function Dasboard() {
   return (
     <>
     <Navbar/>
     <div>Dashboard</div>
+    <Table/>
     </>
   )
 }

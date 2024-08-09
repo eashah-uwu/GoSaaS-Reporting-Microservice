70bad28ca9dbeec76fc156d22f47003a3d24e2b9
naailmahmood <naail.r.mahmood@gmail.com>
2024-08-09 20:06:53 +0500

    Implemented styling + responsiveness to LoginPage, two files commited, LoginPage.tsx and pages.module.css

--- RMS/src/Pages/LoginPage.tsx
+++ RMS/src/Pages/LoginPage.tsx
@@ -1,19 +1,18 @@
 import Navbar from "../Components/Navbar/Navbar";
 import { useState } from "react";
-import { Formik } from "formik";
+import { Formik, FormikHelpers } from "formik";
 import * as yup from "yup";
-import { useNavigate, Link } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { setToken } from '../State/authSlice'; 
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setToken } from "../State/authSlice";
+import axios from "axios";
+import styles from "./pages.module.css";
 
 import {
   Box,
   Button,
-  Divider,
   TextField,
   Typography,
-  useTheme,
   useMediaQuery,
 } from "@mui/material";
 import React from "react";
@@ -28,213 +27,230 @@ const initialValuesLogin = {
   password: "",
 };
 
+interface LoginValues {
+  email: string;
+  password: string;
+}
+
 function LoginPage() {
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
   const [flag, setFlag] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const isNonMobile = useMediaQuery("(min-width:600px)");
+  const isMediumScreen = useMediaQuery("(min-width:960px)");
 
-  const login = async (values: { email: string; password: string }, onSubmitProps: any) => {
+  const login = async (
+    values: LoginValues,
+    onSubmitProps: FormikHelpers<LoginValues>
+  ) => {
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', values);
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        values
+      );
       const token = response.data.token;
       dispatch(setToken(token));
-      localStorage.setItem('token', token); 
-      navigate('/');
+      localStorage.setItem("token", token);
+      navigate("/");
     } catch (error) {
-      // setError('Login failed');
-    
+      setError("Login failed");
     }
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:3000/api/auth/google';
+    window.location.href = "http://localhost:3000/api/auth/google";
   };
 
-  const handleFormSubmit = async (values: { email: string; password: string }, onSubmitProps: any) => {
+  const handleFormSubmit = async (
+    values: LoginValues,
+    onSubmitProps: FormikHelpers<LoginValues>
+  ) => {
     await login(values, onSubmitProps);
   };
 
   return (
     <>
       <Navbar />
-      <Box
-        display="flex"
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "40vh",
-          width: "40%",
-          margin: "auto",
-          borderRadius: "1rem",
-          boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
-          // border: '0.25rem solid #8B0000',
-          borderBottom: "0.25rem solid #8B0000",
-          borderColor: "#8B0000",
-        }}
-      >
-        <img
-          className="logo"
-          src="logo.png"
-          alt="logo"
-          style={{ width: "40%", height: "auto", marginTop: "3rem" }}
-        />
-        <Button
-          fullWidth
-          type="submit"
-          onClick={handleGoogleLogin}
+      <div className={styles.parentContainer}>
+        <Box
+          display="flex"
           sx={{
-            m: "1rem 0",
-            mt: "3rem",
-            p: "1rem",
-            width: "400px",
-            backgroundColor: "#8B0000",
-            color: "#FFFFFF",
-            borderRadius: "0.5rem",
-            "&:hover": {
-              backgroundColor: "#C70039",
-            },
+            marginTop: 8,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "40vh",
+            width: isNonMobile ? "40%" : "90%",
+            margin: "auto",
+            borderRadius: "1rem",
+            boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
+            borderBottom: "0.25rem solid #8B0000",
+            borderColor: "#8B0000",
           }}
         >
-          Sign in with Google
-        </Button>
-
-        <Typography variant="body1" color="error" sx={{ mx: 2 }}>
-          or
-        </Typography>
-
-        {!flag && (
-          <Typography
-            variant="body1"
-            color="error"
+          <img
+            className="logo"
+            src="logo.png"
+            alt="logo"
+            style={{
+              width: isMediumScreen ? "40%" : "60%",
+              height: "auto",
+              marginTop: "3rem",
+            }}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            onClick={handleGoogleLogin}
             sx={{
-              mt: "1rem",
-              mb: "3rem",
+              m: "1rem 0",
+              mt: "3rem",
+              p: "1rem",
+              maxWidth: "80%",
+              backgroundColor: "#8B0000",
+              color: "#FFFFFF",
+              borderRadius: "0.5rem",
+              whiteSpace: "nowrap",
               "&:hover": {
-                color: "#8B0000",
-                cursor: "pointer",
+                backgroundColor: "#C70039",
               },
             }}
-            onClick={() => setFlag(!flag)}
           >
-            Log in with Credentials
-          </Typography>
-        )}
+            Sign in with Google
+          </Button>
 
-        {flag && (
-          <Formik
-            onSubmit={handleFormSubmit}
-            initialValues={initialValuesLogin}
-            validationSchema={loginSchema}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              setFieldValue,
-              resetForm,
-            }) => (
-              <form className="loginForm" onSubmit={handleSubmit}>
-                <Box
-                  component="div"
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  height="90vh"
-                  sx={{
-                    height: flag ? "auto" : 0,
-                    opacity: flag ? 1 : 0,
-                    overflow: "hidden",
-                    transition: "all 0.5s ease-in-out",
-                    marginTop: "1em",
-                  }}
-                >
+          <Typography variant="body1" color="error" sx={{ mx: 2 }}>
+            or
+          </Typography>
+
+          {!flag && (
+            <Typography
+              variant="body1"
+              color="error"
+              sx={{
+                mt: "1rem",
+                mb: "3rem",
+                "&:hover": {
+                  color: "#8B0000",
+                  cursor: "pointer",
+                },
+              }}
+              onClick={() => setFlag(!flag)}
+            >
+              Log in with Credentials
+            </Typography>
+          )}
+
+          {flag && (
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={initialValuesLogin}
+              validationSchema={loginSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+              }) => (
+                <form className="loginForm" onSubmit={handleSubmit}>
                   <Box
-                    display="grid"
-                    gap="30px"
-                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    component="div"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
                     sx={{
-                      "& > div": {
-                        gridColumn: isNonMobile ? undefined : "span 4",
-                      },
+                      height: flag ? "auto" : 0,
+                      opacity: flag ? 1 : 0,
+                      overflow: "hidden",
+                      transition: "all 0.5s ease-in-out",
+                      marginTop: "1em",
                     }}
                   >
-                    <TextField
-                      label="Email"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.email}
-                      name="email"
-                      error={Boolean(touched.email) && Boolean(errors.email)}
-                      helperText={touched.email && errors.email}
+                    <Box
+                      display="grid"
+                      gap="30px"
+                      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
                       sx={{
-                        gridColumn: "span 4",
-                        width: "400px",
-                        pt: "0.5rem",
-                      }}
-                    />
-                    <TextField
-                      label="Password"
-                      type="password"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      value={values.password}
-                      name="password"
-                      error={
-                        Boolean(touched.password) && Boolean(errors.password)
-                      }
-                      helperText={touched.password && errors.password}
-                      sx={{
-                        gridColumn: "span 4",
-                        width: "400px",
-                        pt: "0.5rem",
-                      }}
-                    />
-                  </Box>
-
-                  {error && (
-                    <Box mt="0.5rem">
-                      <Typography variant="body2" color="error">
-                        {error}
-                      </Typography>
-                    </Box>
-                  )}
-
-                  <Box mt="1.5rem">
-                    <Button
-                      fullWidth
-                      type="submit"
-                      sx={{
-                        m: "1rem ",
-                        mb: "5rem",
-                        p: "1rem",
-                        width: "400px",
-                        backgroundColor: "#8B0000",
-                        color: "#FFFFFF",
-                        borderRadius: "0.5rem",
-                        "&:hover": {
-                          backgroundColor: "#C70039",
+                        "& > div": {
+                          gridColumn: isNonMobile ? undefined : "span 4",
                         },
                       }}
                     >
-                      LOGIN
-                    </Button>
+                      <TextField
+                        label="Email"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.email}
+                        name="email"
+                        error={Boolean(touched.email) && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                        sx={{
+                          gridColumn: "span 4",
+                          width: "100%",
+                          pt: "0.5rem",
+                        }}
+                      />
+                      <TextField
+                        label="Password"
+                        type="password"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.password}
+                        name="password"
+                        error={
+                          Boolean(touched.password) && Boolean(errors.password)
+                        }
+                        helperText={touched.password && errors.password}
+                        sx={{
+                          gridColumn: "span 4",
+                          width: "100%",
+                          pt: "0.5rem",
+                        }}
+                      />
+                    </Box>
+
+                    {error && (
+                      <Box mt="0.5rem">
+                        <Typography variant="body2" color="error">
+                          {error}
+                        </Typography>
+                      </Box>
+                    )}
+
+                    <Box mt="1.5rem">
+                      <Button
+                        fullWidth
+                        type="submit"
+                        sx={{
+                          m: "1rem ",
+                          mb: "5rem",
+                          p: "1rem",
+                          maxWidth: "80%",
+                          backgroundColor: "#8B0000",
+                          color: "#FFFFFF",
+                          borderRadius: "0.5rem",
+                          whiteSpace: "nowrap",
+                          "&:hover": {
+                            backgroundColor: "#C70039",
+                          },
+                        }}
+                      >
+                        LOGIN
+                      </Button>
+                    </Box>
                   </Box>
-                </Box>
-              </form>
-            )}
-          </Formik>
-        )}
-      </Box>
+                </form>
+              )}
+            </Formik>
+          )}
+        </Box>
+      </div>
     </>
   );
 }

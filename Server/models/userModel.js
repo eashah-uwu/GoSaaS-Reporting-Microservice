40e5c1ee4d74558e3879c3f088511d173db02590
ali7haider <ali7haider.dev@gmail.com>
2024-08-14 12:43:18 +0500

    Fix bug of column of updatedat

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,19 +1,25 @@
-const knex = require('../config/db/db');
+const knex = require("../config/db/db");
 
 class User {
   static async findByEmail(email) {
-    return knex('User').where({ email }).first();
+    return knex("User").where({ email }).first();
   }
 
   static async findById(id) {
-    return knex('User').where({ userid: id }).first();
+    return knex("User").where({ userid: id }).first();
   }
 
   static async create(userData) {
     const { email, name } = userData;
-    const [newUser] = await knex('User')
-      .insert({ email, name, password: 'default_password',createdat: new Date() })
-      .returning('*');
+    const [newUser] = await knex("User")
+      .insert({
+        email,
+        name,
+        password: "default_password",
+        createdat: new Date(),
+        updatedat: new Date(),
+      })
+      .returning("*");
     return newUser;
   }
 }

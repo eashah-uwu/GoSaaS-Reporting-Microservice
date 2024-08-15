f5429474e6caaf1666adec0253bcef002600f660
ali7haider <ali7haider.dev@gmail.com>
2024-08-15 13:35:51 +0500

    Get all stored procedures from postgres DB

--- Server/config/db/postgresConnection.js
+++ Server/config/db/postgresConnection.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
-const debug = require("debug");
+const debug = require("debug")("postgresql-connection");
 
 class PostgreSQLConnection {
   constructor(config) {
@@ -17,7 +17,7 @@ class PostgreSQLConnection {
       port: config.port,
     };
 
-    console.log("Using PostgreSQL config:", this.config);
+    debug("Using PostgreSQL config:", this.config);
 
     // Initialize knex with the essential PostgreSQL configuration
     this.knex = knex({
@@ -25,13 +25,19 @@ class PostgreSQLConnection {
       connection: this.config,
     });
   }
+
   async testConnection() {
     try {
       await this.knex.raw("SELECT 1+1 AS result");
       console.log("Connection successful");
+
+      const storedProcedures = await this.getStoredProcedures();
+      console.log("Stored procedures:", storedProcedures);
+
       return {
         success: true,
         message: "Connection successful",
+        storedProcedures,
       };
     } catch (error) {
       console.error("Error connecting to PostgreSQL:", error);
@@ -44,35 +50,23 @@ class PostgreSQLConnection {
   }
 
   async getStoredProcedures() {
-    try {
-      const result = await this.knex.raw(`
-        SELECT
-          p.proname AS procedure_name,
-          pg_catalog.pg_get_function_identity_arguments(p.oid) AS parameter_list
-        FROM
-          pg_catalog.pg_proc p
-        JOIN
-          pg_catalog.pg_namespace n ON n.oid = p.pronamespace
-        WHERE
-          n.nspname NOT IN ('pg_catalog', 'information_schema')
-        ORDER BY
-          p.proname;
-      `);
-      return result.rows;
-    } catch (error) {
-      console.error("Error fetching stored procedures:", error);
-      throw error;
-    }
-  }
+    const storedProcedures = await this.knex.raw(`
+    SELECT
+      n.nspname AS schema_name,
+      p.proname AS function_name,
+      l.lanname AS language,
+      p.prokind AS kind
+    FROM
+      pg_catalog.pg_proc p
+    LEFT JOIN pg_catalog.pg_namespace n ON p.pronamespace = n.oid
+    LEFT JOIN pg_catalog.pg_language l ON p.prolang = l.oid
+    WHERE
+      p.prokind = 'p'  -- Filter for procedures only
+    ORDER BY
+      schema_name, function_name;
+  `);
 
-  async closePool() {
-    try {
-      await this.knex.destroy();
-      debug("PostgreSQL connection pool has been closed");
-    } catch (error) {
-      debug("Error closing the PostgreSQL connection pool:", error);
-      throw error;
-    }
+    return storedProcedures.rows;
   }
 }
 

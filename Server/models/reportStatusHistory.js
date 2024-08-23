6a99a3b4442da513c9344bdecdf105661c1300eb
ali7haider <ali7haider.dev@gmail.com>
2024-08-23 15:25:44 +0500

    Fix download pdf bug

--- Server/models/reportStatusHistory.js
+++ Server/models/reportStatusHistory.js
@@ -1,15 +1,12 @@
 const knex = require("../config/db/db");
 
-
 class ReportStatusHistory {
-
-
-    static async findById(id) {
-        return knex("reportstatushistory")
-          .where({ reportstatushistoryid: id })
-          .first()
-          .orderBy("timestamp")
-    }
+  static async findById(id) {
+    return knex("reportstatushistory")
+      .where({ reportstatushistoryid: id })
+      .first()
+      .orderBy("createdat");
+  }
 }
 
 module.exports = ReportStatusHistory;
